f3c67dfe3b51bf15a3137400b6fba2bd76e467c1
Hidenman <elderlutique@gmail.com>
2022-07-21 21:34:49 +0300

    Добавьте файлы проекта.

---
+++ StringToArrayConversion/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringToArrayConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // перетворення рядка в масив слів
+            string str = "hi I am a superman";
+            // поділ рядка по символу пробілу
+             string[] strArray = str.Split(' ');
+            foreach (var words in strArray)
+                Console.WriteLine(words);
+        }
+    }
+}
